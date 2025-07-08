47473ee91282661463b5f760403d4b9d36bc4dc5
Expant <anton532202@gmail.com>
2025-07-08 11:24:56 +0300

    🔧 chore: insert structure from homework-2 branch

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,15 @@
-export default function App() {
-  return <></>;
+import { MainLayout } from "@/shared/layouts/MainLayout";
+import { ThemeProvider } from "@/shared/lib/theme/ThemeProvider";
+import { PostList } from "@/widgets/PostList/PostList";
+import { LayoutHeader } from "@/widgets/LayoutHeader/LayoutHeader";
+import { LayoutFooter } from "@/widgets/LayoutFooter/LayoutFooter";
+
+export function App() {
+  return (
+    <ThemeProvider>
+      <MainLayout header={<LayoutHeader />} footer={<LayoutFooter />}>
+        <PostList />
+      </MainLayout>
+    </ThemeProvider>
+  );
 }
