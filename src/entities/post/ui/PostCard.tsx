dee496b806442895dcf766f335770f6aba12e016
Expant <anton532202@gmail.com>
2025-07-09 23:18:20 +0300

    ✨feat: add CommentList to posts

--- src/entities/post/ui/PostCard.tsx
+++ src/entities/post/ui/PostCard.tsx
@@ -1,15 +1,18 @@
+import type { ReactNode } from "react";
 import styles from "./PostCard.module.css";
 
 type PostCardProps = {
   title: string;
   body: string;
+  children: ReactNode
 };
 
-export function PostCard({ title, body }: PostCardProps) {
+export function PostCard({ title, body, children }: PostCardProps) {
   return (
     <div className={styles.card}>
       <h4 className={styles.title}>{title}</h4>
       <p className={styles.content}>{body}</p>
+      {children}
     </div>
   );
 }
---
+++ src/widgets/CommentList/ui/CommentList.tsx
@@ -0,0 +1,42 @@
+import { useState, useMemo } from "react";
+import { mockComments } from "@/shared/mocks/comments";
+import { Button } from "@/shared/ui/Button/Button";
+import styles from "./CommentList.module.css";
+
+type CommentListProps = {
+  postId: number,
+};
+
+export function CommentList({ postId }: CommentListProps) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleComments = () => setIsExpanded(!isExpanded);
+
+  const comments = mockComments.filter((comment) => comment.postId === postId);
+
+  const commentList = useMemo(() => (
+    comments.map((comment) => (
+      <li className={styles.commentItem} key={comment.id}>
+        <p className={styles.commentItemName}>{comment.name}</p>
+        <p className={styles.commentItemBody}>{comment.body}</p>
+      </li>
+    ))
+  ), [mockComments]);
+
+  return (
+    <div className={styles.commentSection}>
+      <Button 
+        variant="text" 
+        onClick={toggleComments} 
+        className={`${styles.btnTrigger} ${isExpanded ? styles.active : ""}`}
+      >
+        {isExpanded ? "Свернуть комментарии" : `Показать комментарии (${comments.length})`}
+      </Button>
+      {isExpanded && (
+        <ul className={styles.commentList}>
+          {commentList}
+        </ul>
+      )}
+    </div>
+  )
+}
