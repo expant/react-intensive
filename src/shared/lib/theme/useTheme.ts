17443e1fa93ececcd2056a48090a624c954560a8
Expant <anton532202@gmail.com>
2025-07-23 22:58:32 +0300

    refactor: move context to a separate file

--- src/shared/lib/theme/useTheme.ts
+++ src/shared/lib/theme/useTheme.ts
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { ThemeContext } from "./ThemeProvider";
+import { ThemeContext } from "./context";
 
 export function useTheme() {
   return useContext(ThemeContext);
